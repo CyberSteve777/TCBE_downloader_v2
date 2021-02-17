a626e1955041c663063a30d65318926c10c6c879
CyberSteve777 <slacoursedu@gmail.com>
2021-02-17 08:55:14 +0300

    removed temp.py

--- temp.py
+++
@@ -1,9 +0,0 @@
-from github import Github
-import re
-
-
-releases_list = list(Github("CyberSteve777", "5Earth!5").get_user("Ryorama").get_repo(
-            "TerrariaCraft-Bedrock").get_releases())
-for e in releases_list:
-    url = e.raw_data['assets'][0]['browser_download_url']
-    print(re.match('https://github.com/(.+)/(.+)/(.+)/(.+)/(.+)/(.+)', url).groups())
